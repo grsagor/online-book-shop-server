114a40c5f43ffdfa32ab2a28d558a9cecffb2463
grsagor <grsagor08@gmail.com>
2022-11-26 20:37:42 +0600

    mongo connected

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7le8ogp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const categoryCollection = client.db('assignment-12').collection('categories');
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoryCollection.find(query).toArray();
+            res.send(categories);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.log)
+
 
 app.get('/', (req, res) => {
     res.send('server is running');
