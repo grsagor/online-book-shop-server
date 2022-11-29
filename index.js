b5d0a0ae1ab6379bc81501414cad7a8a4ecf782c
grsagor <grsagor08@gmail.com>
2022-11-29 06:23:33 +0600

    jwt token done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { query } = require('express');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -68,6 +70,19 @@ async function run() {
         });
 
         /* Uerssssssssssssssssssssssssssssssssssssssss */
+
+        app.get('/jwt', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+                return res.send({accessToken: token});
+            }
+            console.log(user);
+            res.status(403).send({accessToken: ''});
+        })
+
         app.post('/users', async (req, res) => {
             const body = req.body;
             const result = await usersCollection.insertOne(body);
